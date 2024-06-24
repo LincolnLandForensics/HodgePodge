14a006b916449b3f34b2841fb5f9d4e06e81a0e0
LincolnLandForensics <spamsucks94@gmail.com>
2024-06-24 15:31:44 -0500

    adding text based regex for nasty text sections.

--- pdf2xlsx.py
+++ pdf2xlsx.py
@@ -61,6 +61,7 @@ def main():
     parser.add_argument('-t', '--tables', help='output tables', required=False, action='store_true')
     parser.add_argument('-s', '--subfolders', help='parse subfolders', required=False, action='store_true')
     parser.add_argument('-S', '--split', help='split pdfs', required=False, action='store_true')
+    parser.add_argument('-d', '--table_dump_1', help='parse pdf type 2', required=False, action='store_true')
 
     args = parser.parse_args()
 
@@ -121,32 +122,29 @@ def main():
             msg_blurb = (f'Tables have been extracted and saved to {output_excel_path}')
             msg_blurb_square(msg_blurb, color_green) 
 
+
+    elif args.table_dump_1:
+        output_excel_path = 'output_pdfs.xlsx'
+
+        if not os.path.exists(input_folder):
+            print(f"Error: The directory '{input_folder}' does not exist.")
+        else:
+            msg_blurb = (f"Reading pdf's in {input_folder}")
+            msg_blurb_square(msg_blurb, color_green)             
+        # process_pdfs_in_directory_old(input_folder, output_excel_path) 
+        table_dump1(input_folder, output_excel_path)
+        # process_pdfs_in_directory(input_folder, output_excel_path, tables_out=True, sub_folders=True)
+
+        if tables_out:    
+            msg_blurb = (f'Tables have been extracted and saved to {output_excel_path}')
+            msg_blurb_square(msg_blurb, color_green) 
+
     else:
         usage()
 
     # return 0
 
 # <<<<<<<<<<<<<<<<<<<<<<<<<<   Sub-Routines   >>>>>>>>>>>>>>>>>>>>>>>>>>
-
-def extract_tables_from_pdf(pdf_path):
-    """
-    Extract tables from a given PDF file.
-    
-    Parameters:
-    pdf_path (str): Path to the PDF file.
-    
-    Returns:
-    list of pd.DataFrame: A list of DataFrames, one for each table found in the PDF.
-    """
-    tables = []
-    with pdfplumber.open(pdf_path) as pdf:
-        for page in pdf.pages:
-            extracted_tables = page.extract_tables()
-            for table in extracted_tables:
-                if table:
-                    df = pd.DataFrame(table[1:], columns=table[0])
-                    tables.append(df)
-    return tables
 
 def extract_information(tables):
     """
@@ -164,13 +162,24 @@ def extract_information(tables):
         "Bill To:": None,
         "Ship To:": None,
         "Quantity": None,
+        "Qty": None,
         "Description": None,
         "Price Each": None,
         "Amount": None,
+        "Unit Price": None,
         "Total": None,
         "Product": None,
         "Qty": None,
-        "Unit Price": None,
+        "Column1": None,
+        "Column2": None,
+        "Column3": None,
+        "Column4": None,
+        "Column5": None,
+        "Column6": None,
+        "Column7": None,
+        "Column8": None,
+        "Column9": None,
+        "Column10": None
     }
 
     for table in tables:
@@ -182,30 +191,94 @@ def extract_information(tables):
             break
 
     return data_tables
-    
-    
-def extract_data(tables):
+  
+def extract_tables_from_pdf(pdf_path):
     """
-    Extract the invoice number and date from the tables.
+    Extract tables from a given PDF file.
     
     Parameters:
-    tables (list of pd.DataFrame): List of DataFrames representing tables.
+    pdf_path (str): Path to the PDF file.
     
     Returns:
-    tuple: The invoice number and date if found, otherwise (None, None).
+    list of pd.DataFrame: A list of DataFrames, one for each table found in the PDF.
     """
-    invoice_number = None
-    date = None
-    for table in tables:
-        if 'Invoice #' in table.columns:
-            invoice_number = table.loc[0, 'Invoice #']
-        if 'Date' in table.columns:
-            date = table.loc[0, 'Date']
-        if invoice_number and date:
-            break
-    return invoice_number, date
+    tables = []
+    text, total, date = '', '', ''
+    with pdfplumber.open(pdf_path) as pdf:
+        for page in pdf.pages:
 
-  
+            # extract tables
+            extracted_tables = page.extract_tables()
+            for table in extracted_tables:
+                if table:
+                    df = pd.DataFrame(table[1:], columns=table[0])
+                    tables.append(df)
+
+            # extract text
+            text = page.extract_text()
+            # print(f"Text on page {i+1}:\n{text}")
+            # print(f"Text on page :\n{text}")  # temp
+
+
+            
+            
+    return tables, text
+
+def extract_info_from_text(text, total, date, grand_total, shipping, invoice):
+    disclaimer = ''
+    # total, date = '', ''
+    # Regular expression to find the pattern 'Total $' followed by a number with commas
+    match_time = re.search(r'Total:\s*\$\s*([\d,]+\.\d{2})', text)
+    match_date = re.search(r'Date:\s*(\d{1,2}/\d{1,2}/\d{4})', text)
+    match_total = re.search(r'Total\s*\$([\d,]+\.\d{2})', text)
+    match_total2 = re.search(r'Total:\s*\$([0-9]+\.[0-9]{2})', text)
+    match_shipping = re.search(r'Shipping charge 1 \$([0-9]+\.[0-9]{2})', text)
+    match_invoice = re.search(r'Willow st, suite #B (\d+)', text)
+
+     
+    if match_total:
+        # Remove commas from the number and return it as a float
+        grand_total1 = float(match_total.group(1).replace(',', ''))
+        # print(f'text2 = {text}OOOOOOOOOOOOOOOOOOOOOOOOOOOOOO')  # temp
+        if grand_total1 is None:
+            grand_total1 = ''
+        if grand_total == '':
+            grand_total = grand_total1
+        else:
+            grand_total = (f'{grand_total}\t{grand_total1}')
+        # print(f'grand_total3 = {grand_total}____________________________') # temp   # todo
+    if match_total2:
+        grand_total2 = float(match_total2.group(1).replace(',', ''))
+        if grand_total2 is None:
+            grand_total2 = ''
+        if grand_total == '':
+            grand_total = grand_total2
+        else:
+            grand_total = (f'{grand_total}\t{grand_total2}')            
+        # print(f'grand_total2 = {grand_total2}____________________________') # temp   # todo
+
+    if match_date:
+        if date is None or date == '':
+            date = match_date.group(1)
+            # print(f'date5 = {date}_______________________') # temp    # works
+
+    if 'Customer is responsible for local tobacco tax' in text:
+        disclaimer = 'Customer is responsible for local tobacco tax.'
+    if 'purchased for delivery outside of Illinois' in text:
+        disclaimer = (f'{disclaimer} Tobacco products purchased for delivery outside of Illinois.')
+
+    if match_shipping:
+        if shipping is None or shipping == '':
+            shipping = match_shipping.group(1)
+            
+    if match_invoice:
+        if invoice is None or invoice == '':
+            invoice = match_invoice.group(1)
+            print(f'invoice = {invoice}_____________________')  # temp
+            
+    return disclaimer, total, date, grand_total, shipping, invoice
+
+
 def msg_blurb_square(msg_blurb, color):
     horizontal_line = f"+{'-' * (len(msg_blurb) + 2)}+"
     empty_line = f"| {' ' * (len(msg_blurb))} |"
@@ -240,43 +313,57 @@ def process_pdfs_in_directory(input_folder, output_excel_path):
                 date, invoice, billTo, shipTo, quantity, description = '', '', '', '', '', ''
                 priceEach, subtotal, amount, total, billToState, shipToState = '', '', '', '', '', ''
                 customer, state, year, month, day, folder = '', '', '', '', '', ''
-                
+                grand_total, shipping = '', ''
                 row_data = {}
                 pdf_path = os.path.join(root, filename)
                 folder = pdf_path
-                tables = extract_tables_from_pdf(pdf_path)
+                tables, text = extract_tables_from_pdf(pdf_path)
+                # print(f'text = {text}') # temp
+                disclaimer, total, date, grand_total, shipping, invoice = extract_info_from_text(text, total, date, grand_total, shipping, invoice)
+                if total:
+                    print(f"Found total value : ${total}")
+                # else:
+                    # print(f"total not found")                
+
                 all_tables.extend(tables)
 
                 data_tables = extract_information(tables)
-            
+                # print(f'date2 = {date}') # temp
                 for key, value in data_tables.items():
                     if value:
                         if key == "Amount":
                             amount = value
-
                         elif key == "Date":
                             date = value
                         elif key == "Invoice #":
                             invoice = value
                         elif key == "Bill To:":
                             billTo = value
-
                         elif key == "Ship To:":
                             shipTo = value
                         elif key == "Quantity":
                             quantity = value
+                            if quantity is None:
+                                quantity == ''
+                        elif key == "Qty" and quantity == '' :
+                            quantity = value                            
                         elif key == "Qty":
-                            qty = value
+                            quantity = value
                         elif key == "Description":
+                            description = value
+                            if description is None:
+                                description == ''                            
+                        elif key == "Product" and description == '':
                             description = value
                         elif key == "Price Each":
                             priceEach = value
+                            if priceEach is None:
+                                priceEach == ''
+                        elif key == "Unit Price" and priceEach == '':
+                            priceEach = value
+
                         elif key == "Total":
                             total = value
-                        elif key == "Product":
-                            product = value
-                        elif key == "Unit Price":
-                            unit_price = value
 
 
                 pattern = r',\s([A-Z]{2})\s\d{5}'
@@ -306,9 +393,10 @@ def process_pdfs_in_directory(input_folder, output_excel_path):
 
                 total = sum(float(value4.replace(',', '')) for value4 in match4)    
                 total = round(float(total), 2)
-
+                
                 try:
                     if date.count('/') == 2:
+                        # print(f'date3 = {date}') # temp
                         input_date = datetime.strptime(date, '%m/%d/%Y')
                         date = input_date.strftime('%Y-%m-%d')
                         year = input_date.strftime('%Y')
@@ -316,7 +404,7 @@ def process_pdfs_in_directory(input_folder, output_excel_path):
                         day = input_date.strftime('%d')
                 except Exception as e:
                     print(f"Error converting date: {str(e)}")
-
+                # print(f'date4 = {date}') # temp
                 row_data["filename"] = filename
                 row_data["date"] = date
                 row_data["invoice"] = invoice
@@ -337,6 +425,9 @@ def process_pdfs_in_directory(input_folder, output_excel_path):
                 row_data["day"] = day
                 row_data["folder"] = folder
                 row_data["seller"] = seller
+                row_data["disclaimer"] = disclaimer
+                row_data["grand_total"] = grand_total
+                row_data["shipping"] = shipping
 
                 print(f'{filename}    {total}')   # test
                 data.append(row_data)
@@ -350,7 +441,6 @@ def process_pdfs_in_directory(input_folder, output_excel_path):
 
     write_xlsx(data)
 
-
 def split_pdfs(input_folder, output_split_folder):
     '''
     Function to split PDFs into single-page PDFs
@@ -359,7 +449,12 @@ def split_pdfs(input_folder, output_split_folder):
     if not os.path.exists(output_split_folder):
         os.makedirs(output_split_folder)
 
+        msg_blurb = (f'Creating split pdf folder: {output_split_folder}')
+        msg_blurb_square(msg_blurb, color_green)
 
+    msg_blurb = (f"Splitting PDFs from {input_folder}")
+    msg_blurb_square(msg_blurb, color_green)
+        
     for root, dirs, files in os.walk(input_folder):
         for filename in files:
             if filename.lower().endswith('.pdf'):  # Check for .pdf extension in a case-insensitive manner
@@ -383,8 +478,51 @@ def split_pdfs(input_folder, output_split_folder):
                             writer.write(output_pdf)
                         
                         print(f"Created: {output_path}")
+
+    msg_blurb = (f"writing split pdfs to {output_split_folder}")
+    msg_blurb_square(msg_blurb, color_green)
+
+def table_dump1(input_folder, output_excel_path):
+    """
+    Loop through all PDFs in the specified directory (and optionally in subdirectories), extract tables, and export them to an Excel sheet.
     
-    
+    Parameters:
+    input_folder (str): Path to the directory containing PDF files.
+    output_excel_path (str): Path where the output Excel file will be saved.
+    tables_out (bool): Whether to save the extracted tables into separate sheets in the Excel file.
+    sub_folders (bool): Whether to include PDFs from subdirectories.
+    """
+    if tables_out:
+        writer = pd.ExcelWriter(output_excel_path, engine='openpyxl')
+        
+    all_tables = []
+    # data = []
+    seller = input("SELLER: ")
+    # Walk through directories and subdirectories if sub_folders is True
+    for root, dirs, files in os.walk(input_folder) if sub_folders else [(input_folder, [], os.listdir(input_folder))]:
+        for filename in files:
+            if filename.lower().endswith('.pdf'):
+                date, invoice, billTo, shipTo, quantity, description = '', '', '', '', '', ''
+                priceEach, subtotal, amount, total, billToState, shipToState = '', '', '', '', '', ''
+                customer, state, year, month, day, folder = '', '', '', '', '', ''
+                
+                row_data = {}
+                pdf_path = os.path.join(root, filename)
+                folder = pdf_path
+                # tables = extract_tables_from_pdf(pdf_path)
+                tables, text = extract_tables_from_pdf(pdf_path)
+                all_tables.extend(tables)
+
+
+    if tables_out:
+        for idx, table in enumerate(all_tables):
+            sheet_name = f'Table{idx+1}'
+            table.to_excel(writer, sheet_name=sheet_name, index=False)
+        writer.save()
+        writer.close()
+
+    # write_xlsx(data)    
+
 def write_xlsx(data):
 
     global workbook
@@ -401,13 +539,13 @@ def write_xlsx(data):
     headers = [
         "filename", "date", "invoice", "customer", "total", "billTo", "shipTo", "quantity", "description"
         , "priceEach", "amount", "billToState", "shipToState", "year", "month", "day", "folder", "seller"
+        , "disclaimer", "grand_total", "shipping"
     ]
-
-    
+ 
     for col_index, header in enumerate(headers):
         cell = worksheet.cell(row=1, column=col_index + 1)
         cell.value = header
-        if col_index in range(18):  # range(18) generates numbers from 0 to 25 inclusive orange
+        if col_index in range(21):  # range(18) generates numbers from 0 to 25 inclusive orange
             fill = PatternFill(start_color="FFA500", end_color="FFA500", fill_type="solid") # orange
             cell.fill = fill
 
@@ -430,9 +568,9 @@ def write_xlsx(data):
     worksheet.column_dimensions['P'].width = 12  # 
     worksheet.column_dimensions['Q'].width = 30  # folder
     worksheet.column_dimensions['R'].width = 15  # seller
-
-
-
+    worksheet.column_dimensions['S'].width = 11  # disclaimer
+    worksheet.column_dimensions['T'].width = 11  # grand_total
+    worksheet.column_dimensions['U'].width = 9  # shipping
 
     for row_index, row_data in enumerate(data):
         for col_index, col_name in enumerate(headers):
@@ -443,7 +581,6 @@ def write_xlsx(data):
                 worksheet.cell(row=row_index+2, column=col_index+1).value = cell_data
             except Exception as e:
                 print(f"{color_red}Error printing line: {str(e)}{color_reset}")
-
 
     msg_blurb = (f'Writing to {output_xlsx}')
     msg_blurb_square(msg_blurb, color_green)
@@ -464,7 +601,6 @@ def usage():
     # print(f'    {file} -p -t')     
     print(f'    {file} -p -I pdfs -O invoices_.xlsx ')     
     print(f'    {file} -S   # split pdfs into single page into the {output_split_folder} folder')     
-
 
 if __name__ == "__main__":
 
@@ -492,6 +628,3 @@ can be adapated to different table column names
 """
 
 # <<<<<<<<<<<<<<<<<<<<<<<<<<      The End        >>>>>>>>>>>>>>>>>>>>>>>>>>
-
-
-
