34c69d994976546fe73cca4dc53d67ee7e87ff7c
LincolnLandForensics <spamsucks94@gmail.com>
2024-12-24 08:35:11 -0600

    .

--- ForensicsReporter/ForensicsReporter.py
+++ ForensicsReporter/ForensicsReporter.py
@@ -5,9 +5,9 @@
 # <<<<<<<<<<<<<<<<<<<<<<<<<<     Change Me       >>>>>>>>>>>>>>>>>>>>>>>>>>
 # change this section with your details
 global agency
-agency = "IDOR" # ISP, MWW
+agency = "MWW" # ISP, MWW
 global agencyFull
-agencyFull = "Illinois Department of Revenue"   # Ministry of Wacky Walks
+agencyFull = "Ministry of Wacky Walks"   # Ministry of Wacky Walks
 global divisionFull
 divisionFull = "Bureau of Criminal Investigations" # Criminal Investigation Division
 
