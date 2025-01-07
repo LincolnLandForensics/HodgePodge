de63e824652deb2dcc5019d54d94a6d9380f4f9e
LincolnLandForensics <spamsucks94@gmail.com>
2025-01-07 12:40:19 -0600

    .

--- NCMEC/NCMEC_PDF_parser.py
+++ NCMEC/NCMEC_PDF_parser.py
@@ -25,19 +25,13 @@ except Exception as e:
 
 sys.stdout.reconfigure(encoding='utf-8')  # Python 3.7+
 
-
 # <<<<<<<<<<<<<<<<<<<<<<<<<<      Pre-Sets       >>>>>>>>>>>>>>>>>>>>>>>>>>
-
 
 author = 'LincolnLandForensics'
 description = "read .zip and .pdfs and extract out NCMEC intel"
 version = '0.1.8'
 
-
-
 # <<<<<<<<<<<<<<<<<<<<<<<<<<    Main    >>>>>>>>>>>>>>>>>>>>>>>>>>
-
-
 
 def main():
     """
@@ -89,6 +83,18 @@ def main():
 # <<<<<<<<<<<<<<<<<<<<<<<<<<   Sub-Routines   >>>>>>>>>>>>>>>>>>>>>>>>>>
 
 
+def check_pdf_readability(file_path):
+    try:
+        with pdfplumber.open(file_path) as pdf:
+            for page_number, page in enumerate(pdf.pages, start=1):
+                text = page.extract_text()
+                if text and text.strip():  # If any page has text, return
+                    print(f"{file_path} is readable.")
+                    return
+        print(f"{file_path} needs to be OCR'd to read it.")
+    except Exception as e:
+        print(f"Error processing {file_path}: {e}")
+        
 def cleanup_email(email_list):
     whitelist = ['.gov', 'ncmec.org', '.Upon']
     return [
@@ -103,7 +109,8 @@ def process_pdfs_and_unzip(input_folder, output_folder):
     unzip_all_in_folder(input_folder, output_folder)
 
     today = datetime.now().strftime("%Y-%m-%d")
-    word_list, sentence_list, email_list, md5_list, ip_list, phone_list, user_list = ([] for _ in range(7))
+    # word_list, sentence_list, email_list, md5_list, ip_list, phone_list, user_list = ([] for _ in range(7))
+    word_list, sentence_list, email_list, file_list, md5_list, ip_list, phone_list, user_list = ([] for _ in range(8))
 
     email_pattern = re.compile(r'[\w.-]+@[\w.-]+\.[a-zA-Z]{2,6}')
     md5_pattern = re.compile(r'\b[a-fA-F0-9]{32}\b')
@@ -115,9 +122,10 @@ def process_pdfs_and_unzip(input_folder, output_folder):
         
     def process_pdf(file_path):
 
-        nonlocal word_list, sentence_list, email_list, md5_list, ip_list, phone_list, user_list
+        nonlocal word_list, sentence_list, email_list, file_list, md5_list, ip_list, phone_list, user_list
 
         try:
+            text = ''
             with pdfplumber.open(file_path) as pdf:
                 for page in pdf.pages:
                     text = page.extract_text() or ""
@@ -131,6 +139,8 @@ def process_pdfs_and_unzip(input_folder, output_folder):
                             user_list.append(sentence.split(':', 1)[1].strip())
                         elif sentence.startswith('ESP User ID: '):
                             user_list.append(sentence.replace('ESP User ID: ', '').strip())
+                        elif sentence.startswith('Filename:'):
+                            file_list.append(sentence.replace('Filename:', '').strip())
                         elif sentence.startswith('IP Address: ') and ' (Login)' in sentence:
                             ip_list.append(sentence.replace('IP Address: ', '').split(' ')[0].strip())
                         elif sentence.startswith('Phone: +') and ' (Verified' in sentence:
@@ -147,6 +157,12 @@ def process_pdfs_and_unzip(input_folder, output_folder):
         except Exception as e:
             print(f"Error processing {file_path}: {str(e)}")
 
+        # test for pdf's that need to be OCR'd first
+        if text and text.strip():  # If any page has text, return
+            blah = 'blah'
+        else:
+            print(f"{file_path} needs to be OCR'd to read it.")  
+
 
     for root, _, files in os.walk(input_folder):
         for file in files:
@@ -161,6 +177,7 @@ def process_pdfs_and_unzip(input_folder, output_folder):
     export_data = {
         # f"sentences_{today}.txt": unique_sorted(sentence_list),
         f"emails_{today}.txt": unique_sorted(email_list),
+        f"files_{today}.txt": unique_sorted(file_list),        
         f"md5_hashes_{today}.txt": unique_sorted(md5_list),
         f"ips_{today}.txt": unique_sorted(ip_list),
         f"phones_{today}.txt": unique_sorted(phone_list),
@@ -169,14 +186,17 @@ def process_pdfs_and_unzip(input_folder, output_folder):
 
     for filename, data in export_data.items():
         with open(os.path.join(output_folder, filename), "w", encoding="utf-8", errors="replace") as file:
+            if "md5_hashes_" in filename:  # Check if this is the MD5 file
+                file.write("MD5\n")       # Prepend "MD5" as the first line
+
             file.write("\n".join(data))
 
     if pdfs_parsed != 0:
         # print(f"\n{pdfs_parsed} PDfs parsed")
         print(f"\nPDfs parsed")
 
-
     print(f"\n	{len(unique_sorted(email_list))} emails")
+    print(f"\t{len(unique_sorted(file_list))} files")    
     print(f"\t{len(unique_sorted(ip_list))} IPs")
     print(f"\t{len(unique_sorted(md5_list))} MD5 hashes")
     print(f"\t{len(unique_sorted(phone_list))} phone numbers")
@@ -234,8 +254,6 @@ def Usage():
     print("\t" + sys.argv[0] + " -p")
     print("\t" + sys.argv[0] + " -p -I C:\\Forensics\\scripts\\python\\NCMEC -O C:\\Forensics\\scripts\\python\\NCMEC\\_output")
 
-
-
 if __name__ == '__main__':
     main()
     
@@ -243,6 +261,10 @@ if __name__ == '__main__':
 # <<<<<<<<<<<<<<<<<<<<<<<<<< Revision History >>>>>>>>>>>>>>>>>>>>>>>>>>
 
 """
+0.2.0 - added an additional output file with filenames?
+0.1.9 - automatically create outputful folder inside the input folder, if it doesn't exist and -O isn't specified. 
+0.1.8 - add "MD5" as the first line of the hash file (X-Ways requires it.) 
+0.1.7 - fixed -O option to work in specified folder
 0.1.3 - handle udf8 characters (like emoji), removed cybertip number from users.txt
 0.1.2 - stopped exporting sentences and words, added menu, added 'ESP User ID: ' to users.txt
 0.1.0 - working prototype
@@ -251,19 +273,16 @@ if __name__ == '__main__':
 # <<<<<<<<<<<<<<<<<<<<<<<<<< Future Wishlist  >>>>>>>>>>>>>>>>>>>>>>>>>>
 
 """
-if -O is other than default, it gives blank output files. Works with full path instead of \temp (for example)
-works with temp but not \temp
-
+detect if pdf needs to be OCR'd. 
 split ipv6 in half and add them to the ip list
 delete the unzipped folders at the end of the script 
-
 
 """
 
 # <<<<<<<<<<<<<<<<<<<<<<<<<<      notes            >>>>>>>>>>>>>>>>>>>>>>>>>>
 
 """
-This assumes NCMEC .zip files have no password
+This assumes NCMEC .zip files have not password
 IP and MD5 don't have to be NCMEC specific
 .7z extraction can be easily added if needed
 
